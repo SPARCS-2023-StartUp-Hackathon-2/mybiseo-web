387e4a0f156e297ba8b9628c976b61932125f695
enjoy301 <eunjong147@pusan.ac.kr>
2023-01-27 21:00:05 +0900

    feat: styled-components 적용

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+const HomeBox = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+`;
+
+const Title = styled.div`
+  color: white;
+`;
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeBox>
+      <Title>Home</Title>
+    </HomeBox>
   );
 };
